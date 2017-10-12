776728950115d25bf6b103aeb860d9dcebd3eca7
jayunderwood2011 <jayunderwood2011@hotmail.com>
2017-10-12 11:43:21 +0100

    update screenshots for the admin dashboard profile

--- src/run.py
+++ src/run.py
@@ -13,4 +13,5 @@ def create_shell():
 
 if  __name__ == '__main__':
     manager.run() # start the flask web server by running the command: 'python run.py runserver' in the command line
+                              # url link: http://127.0.0.1:5000/<some url page >
 
