0119de77993a8fc7b33c1bbc7744e68f01617c5e
jayunderwood2011 <jayunderwood2011@hotmail.com>
2017-11-14 02:38:19 +0000

    completed the third stage for the user registration located in the registration views

--- src/users/registration/form.py
+++ src/users/registration/form.py
@@ -1,7 +1,10 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, validators
 from wtforms.fields.html5 import EmailField
+from wtforms.validators import ValidationError
+import re
 
+_match = re.compile(r'^[A-Za-z0-9_]+$')
 
 class RegistrationForm(Form):
     """The RegistrationForm allows the user to enter the fields needed to register the application in the GUI"""
@@ -18,4 +21,18 @@ class RegistrationForm(Form):
     confirm = PasswordField('Repeat password')
     author_name = StringField('Author name', validators=[validators.DataRequired(), validators.Length(min=3, max=80)])
 
+    @staticmethod
+    def _check_name_value(name):
+        found = _match.search(name)
+        if not found:
+            raise ValidationError("The name can contain an uppercase, a lowercase, digits(0-9) or an underscore"
+                                  " but must not contain any special characters!")
 
+    def validate_first_name(form, field):
+        RegistrationForm._check_name_value(form.first_name.data)
+
+    def validate_last_name(form, field):
+        RegistrationForm._check_name_value(form.last_name.data)
+
+    def validate_username(form, field):
+        RegistrationForm._check_name_value(form.username.data)
