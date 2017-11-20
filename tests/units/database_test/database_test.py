fc8173a7da213284ab32fc5b5c5c99ef4a396af0
jayunderwood2011 <jayunderwood2011@hotmail.com>
2017-11-20 23:37:59 +0000

    Refactor the registration test case in two test cases Database and registration

--- tests/units/database_test/database_test.py
+++ tests/units/database_test/database_test.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
 
-from users.users.users import User
-from users.utils.generator.id_generator import gen_id
+from src.users.records.database.database import Database
+from src.users.utils.generator.id_generator import gen_id
 
 
-class RegistrationTest(TestCase):
+class DataBaseTest(TestCase):
 
     def setUpClass(cls):
         """"""
@@ -16,6 +16,7 @@ class RegistrationTest(TestCase):
                        "password": 'password',
                        "configuration_codes" : gen_id()
         }
+        Database.db_init()
 
     def test_if_a_single_user_details_can_be_saved_to_the_database__should_save_the_user_details_to_database(self):
         pass
@@ -26,7 +27,7 @@ class RegistrationTest(TestCase):
     def test_if_three_user_details_can_be_saved_to_the_database__should_save_three_user_details_to_database(self):
         pass
 
-    def test_if_a_user_data_can_be_retreived_by_using_a_correct_username__should_retreive_all_data_belonging_to_the_user(self):
+    def test_if_a_user_data_can_be_retreived_from_database_using_a_correct_username__should_retreive_all_data_belonging_to_the_user(self):
         pass
 
     def test_if_user_data_can_retreived_from_the_database_if_an_invalid_username_is_entered__should_return_a_none(self):
@@ -34,22 +35,3 @@ class RegistrationTest(TestCase):
 
     def test_if_user_data_can_be_retrieived_from_database_with_a_correct_email_address__should_return_all_data_belonging_to_the_user(self):
         pass
-
-    def test_if_user_data_is_converted_to_json__should_return_a_json_object(self):
-        pass
-
-    def test_if_user_account_verification_code_is_generated__should_generate_a_random_generated_string(cls):
-        pass
-
-    def test_if_an_assertion_error_is_raised_if_no_details_is_entered(self):
-        pass
-
-    def test_if_user_blog_id_code_is_generated__should_generate_a_random_generated_string(cls):
-        pass
-
-    def test_if_user_author_id_code_is_generated__should_generate_a_random_generated_string(cls):
-        pass
-
-    def test_repr_when_data_is_passed_to_the_object__should_return_a_string_with_first_name_and_a_last_name(self):
-        pass
-
---
+++ tests/units/registration_test/register_user_test.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+
+
+class RegisterationTest(TestCase):
+
+    def setUpClass(cls):
+        pass
+
+    def test_if_user_data_is_converted_to_json__should_return_a_json_object(self):
+        pass
+
+    def test_if_user_account_verification_code_is_generated__should_generate_a_random_generated_string(cls):
+        pass
+
+    def test_if_an_assertion_error_is_raised_if_no_details_is_entered(self):
+        pass
+
+    def test_if_user_blog_id_code_is_generated__should_generate_a_random_generated_string(cls):
+        pass
+
+    def test_if_user_author_id_code_is_generated__should_generate_a_random_generated_string(cls):
+        pass
+
+    def test_repr_when_data_is_passed_to_the_object__should_return_a_string_with_first_name_and_a_last_name(self):
+        pass
