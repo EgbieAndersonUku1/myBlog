78da411801754cb63c06e5369ce515773a609a25
jayunderwood2011 <jayunderwood2011@hotmail.com>
2018-02-09 10:19:36 +0000

    Refactor a class in the utils/generator that allows files of different images to be generated.

--- src/users/utils/generator/gen_img_sizes.py
+++ src/users/utils/generator/gen_img_sizes.py
@@ -1,3 +1,4 @@
+
 from time import time as time_stamp
 from PIL import Image
 import os
@@ -36,10 +37,10 @@ class Imager(object):
 
         Imager._check_img_extension(ext)
 
-        img = self._crop_from_center(self._get_img_copy())
+        self._img = self._crop_from_center()
         file_path = self._create_file_path(self.folder_path, self._create_img_file_name('orig_image', ext))
-        img.save(file_path)
-        return self._gen_sizes(ext, img)
+        self._img.save(file_path)
+        return self._gen_sizes(ext)
 
     @staticmethod
     def _check_img_extension(ext):
@@ -52,16 +53,12 @@ class Imager(object):
         """fixes the image extention if it is incorrect"""
         return ext if ext.startswith('.') else "." + ext
 
-    def _get_img_copy(self):
-        """Creates and returns a copy using the original image"""
-        return self._img.copy()
-
-    def _crop_from_center(self, img):
+    def _crop_from_center(self):
         """Takes an image and crop it from the center"""
         dst_landscape = 1 > self._img.width / self._img.height
         wh = self._img.width if dst_landscape else self._img.height
 
-        return img.crop((int((self._img.width - wh) / 2), int((self._img.height - wh) / 2), int(wh), int(wh)))
+        return self._img.crop((int((self._img.width - wh) / 2), int((self._img.height - wh) / 2), int(wh), int(wh)))
 
     @staticmethod
     def _create_file_path(folder_path, file_name):
@@ -72,7 +69,7 @@ class Imager(object):
         """Creates a file name for the image"""
 
         self._is_image_ext_correct(file_ext)
-        return "{}_{}_{}.{}".format(self._image_id, time_stamp(), size, file_ext)
+        return "{}_{}.{}".format(self._image_id, size, file_ext)
 
     @staticmethod
     def _is_image_ext_correct(ext):
@@ -80,23 +77,23 @@ class Imager(object):
         file_ext = ['png', 'jpeg', 'jpg', 'gif']
         assert ext.split(".")[-1].lower() in file_ext, 'File format must be either PNG, JPEG, JPG or GIF'
 
-    def _gen_sizes(self, ext, img):
+    def _gen_sizes(self, ext):
         """Takes img extension and image object and generates five images of differents sizes"""
 
         img_sizes = {'xsmall': 50, 'small': 70, 'medium': 200, 'large': 400, 'xlarge': 600}
 
         for size_type in img_sizes:
             size = img_sizes.get(size_type)
-            new_img = self._resize_img(size, size, img)
+            new_img = self._resize_img(size, size)
             img_path = Imager._create_file_path(self.folder_path, self._create_img_file_name(size_type, ext))
             new_img.save(img_path)
-        return time_stamp()
+        return str(time_stamp())
 
-    def _resize_img(self, width, height, img):
+    def _resize_img(self, width, height):
         """Takes a width and a height and resizes the image to those attributes.
            Returns and image object
         """
-        return img.resize((int(width), int(height)))
+        return self._img.resize((int(width), int(height)))
 
     @staticmethod
     def _does_folder_path_exist(folder_path):
