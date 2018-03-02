f5c8b9cbcda7899dba58ce7c767c09d3d98a374d
jayunderwood2011 <jayunderwood2011@hotmail.com>
2018-03-02 14:23:43 +0000

    Deleted a lines of code

--- src/users/comments/views.py
+++ src/users/comments/views.py
@@ -12,14 +12,12 @@ comment_app = Blueprint("comment_app", __name__, url_prefix="/comments")
 def delete_comment(blog_id, post_id, comment_id):
     """"""
     post, blog = None, UserBlog()
-    blog.get_blog(blog_id)
-
     child_blog = blog.get_blog(blog_id)
 
     if child_blog:
         post = child_blog.Post.get_post_by_id(post_id)
     if post:
-       post.Comment.delete_comment(comment_id)
+        post.Comment.delete_comment(comment_id)
 
     return redirect(url_for("posts_app.post_permalink", blog_id=blog_id, post_id=post_id, comment_id=comment_id))
 
@@ -28,4 +26,4 @@ def delete_comment(blog_id, post_id, comment_id):
 def edit_comment(user_id, blog_id, post_id, comment_id):
     # ToDo
     # Write the function needed to edit comments.
-    pass
+    pass 
